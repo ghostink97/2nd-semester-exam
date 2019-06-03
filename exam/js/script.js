592c3df760f61da4206b641f017f7fc7dfbc3829
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-06-04 00:08:54 +0200

    JS- removed comments

--- exam/js/script.js
+++ exam/js/script.js
@@ -1,16 +1,15 @@
-
 // Mobile navigation
 
 const mq = window.matchMedia("(max-width: 719px)");
 
-if(mq.matches) {
-   const mobile = document.getElementsByClassName("container");
+if (mq.matches) {
+    const mobile = document.getElementsByClassName("container");
     const subMenu = document.querySelectorAll(".submenu-Mobile");
-    const dropSubmenu =document. querySelectorAll(".drop-submenu");
+    const dropSubmenu = document.querySelectorAll(".drop-submenu");
 
 
-    for ( let i = 0; i < dropSubmenu.length; i++) {
-        dropSubmenu[i].addEventListener('click', function(){
+    for (let i = 0; i < dropSubmenu.length; i++) {
+        dropSubmenu[i].addEventListener('click', function () {
             subMenu[i].classList.toggle("display-block");
         });
     }
@@ -21,31 +20,30 @@ if(mq.matches) {
 // Inspired by  www.w3schools.com
 
 function openMobileNav() {
-  document.getElementById("mobileNav").style.width = "100%";
+    document.getElementById("mobileNav").style.width = "100%";
 }
 
 function closeMobileNav() {
-  document.getElementById("mobileNav").style.width = "0%";
+    document.getElementById("mobileNav").style.width = "0%";
 }
 
 
-// // Used  in previous project
-// // Scroll down more then 25px - see the button
-// window.onscroll = function() {scroll()};
+// Used  in previous project
+// Scroll down more then 25px - see the button
+window.onscroll = function () {
+    scroll()
+};
 
-// function scroll() {
-//   if (document.documentElement.scrollTop > 25) {
-//     document.getElementById("topBtn").style.display = "block";
-//   } else {
-//     document.getElementById("topBtn").style.display = "none";
-//   }
-// }
-
-
-// // Click the button - come back on the top of the page
-// function btnTopFunction() {
-//   document.documentElement.scrollTop = 0;
-// }
+function scroll() {
+    if (document.documentElement.scrollTop > 25) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
 
 
-
+// Click the button - come back on the top of the page
+function btnTopFunction() {
+    document.documentElement.scrollTop = 0;
+}
