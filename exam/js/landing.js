f40774632775515f46459c5f8ee786580a5f3042
ourangelofsorrows <angelezrameyer@gmail.com>
2019-06-03 09:21:19 +0200

    i made kasdlækdsflæskd changes

---
+++ exam/js/landing.js
@@ -0,0 +1,4 @@
+//enter button on landing page
+document.querySelector("#enterBtn").addEventListener("click", function(){
+    window.open("home.html", "_self");
+  } );
--- exam/js/script.js
+++ exam/js/script.js
@@ -1,8 +1,3 @@
-//enter button on landing page
-document.querySelector("#enterBtn").addEventListener("click", function(){
-  window.open("home.html", "_self");
-} );
-
 // Mobile navigation
 
 const mq = window.matchMedia("(max-width: 719px)");
@@ -19,6 +14,36 @@ if(mq.matches) {
         });
     }
 
+}
+
+
+// Used  in previous projects
+// Inspired by  www.w3schools.com
+
+function openMobileNav() {
+  document.getElementById("mobileNav").style.width = "100%";
+}
+
+function closeMobileNav() {
+  document.getElementById("mobileNav").style.width = "0%";
+}
+
+
+// Used  in previous projects
+// Scroll down more then 25px - see the button
+window.onscroll = function() {scroll()};
+
+function scroll() {
+  if (document.documentElement.scrollTop > 25) {
+    document.getElementById("topBtn").style.display = "block";
+  } else {
+    document.getElementById("topBtn").style.display = "none";
+  }
+}
+
+// Click the button - come back on the top of the page
+function btnTopFunction() {
+  document.documentElement.scrollTop = 0;
 }
 
 
